903bcfaae337f08640c9d1a3c38ad9e015e6af88
kaajTV <anton.holmgren3@hotmail.com>
2022-09-08 15:27:50 +0200

    Update

--- navbarstyle1/src/components/navbar/Navbar.jsx
+++ navbarstyle1/src/components/navbar/Navbar.jsx
@@ -18,6 +18,48 @@ const Menu = () => (
 const Navbar = () => {
     const [toggleMenu, setToggleMenu] = useState(false);
     const [toggleLogin, setToggleLogin] = useState(false);
+    const [errorMessages, setErrorMessages] = useState({});
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+    const database = [
+        {
+            username: "username1",
+            password: "password1"
+        },
+        {
+            username: "username2",
+            password: "password2"
+        }
+    ];
+
+    const errors = {
+        uname: "invalid username",
+        pass: "invalid password"
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        var { uname, pass } = document.forms[0];
+
+        const userData = database.find((user) => user.username === uname.value);
+
+        if (userData) {
+            if (userData.password !== pass.value) {
+                setErrorMessages({ name: "pass", message: errors.pass });
+            } else {
+                setIsSubmitted(true);
+            }
+        } else {
+            setErrorMessages({ name: "uname", message: errors.uname });
+        }
+    };
+
+    const renderErrorMessage = (name) =>
+        name === errorMessages.name && (
+            <div className="error">{errorMessages.message}</div>
+        );
+    
     return (
         <div className="project__navbar">
             <div className="project__navbar-links">
@@ -30,13 +72,41 @@ const Navbar = () => {
             </div>
             <div className="project__navbar-sign">
                 {toggleLogin
-                    ? <RiCloseLine color="#fff" size={35}  onClick={() => setToggleLogin(false)} />
+                    ? <p onClick={() => setToggleLogin(false)}>Sign in</p>
                     : <p onClick={() => setToggleLogin(true)}>Sign in</p>
                 }
                 {toggleLogin && (
-                    <Login />
+                    <div className="project__login">
+                        <div className="project__login-overlay" onClick={() => setToggleLogin(false)}></div>
+                        <form className="project__login-form" onSubmit={handleSubmit}>
+                            <div className="project__login-form_close">
+                                <RiCloseLine color="#fff" size={35} onClick={() => setToggleLogin(false)} />
+                            </div>
+                            <div className="project__login-form_group success">
+                                {/* <label htmlFor="email">Email</label>
+                                <input pattern="[a-zA-Z0-9._+-]+@[a-zA-Z0-9*-]+\.[a-zA-Z]{2,}" type="email" title="must be valid" id="email" required />
+                                <span className="msg">Valid email</span> */}
+                                <label htmlFor="">Username </label>
+                                <input type="text" name="uname" id="username" required />
+                                {renderErrorMessage("uname")}
+                            </div>
+                            <div className="project__login-form_group error">
+                                {/* <label htmlFor="password">Password</label> */}
+                                {/* <input pattern="[a-zA-Z0-9._+-!]{8,}" title="Must be at least 8 characters" type="password" id="password" required /> */}
+                                <label htmlFor="">Password </label>
+                                <input type="password" name="pass" id="password" required />
+                                {renderErrorMessage("pass")}
+                                <span className="project__login-form_group-inputhint">
+                                    Must be at least 8 characters
+                                </span>
+                                {/* <span className="msg">Incorrect password</span> */}
+                            </div>
+                            <input type="submit" value="Login" />
+                            {/* <button type="submit">Login</button> */}
+                        </form>
+                        {/* {isSubmitted ? toggleLogin : toggleLogin} */}
+                    </div>
                 )}
-                {/* <p>Sign in</p> */}
                 <button>Sign up</button>
             </div>
             <div className="project__navbar-menu">
@@ -46,15 +116,11 @@ const Navbar = () => {
                 }
                 {toggleMenu && (
                     // <Sidebar />
-                     <div className="project__navbar-menu_container scale-up-center">
-                         <div className="project__navbar-menu_container-links">
-                             <Sidebar />
-                             {/* <div className="project__navbar-menu_container-links-sign">
-                                 <p>Sign in</p>
-                                 <button>Sign up</button>
-                             </div> */}
-                         </div>
-                     </div>
+                    <div className="project__navbar-menu_container scale-up-center">
+                        <div className="project__navbar-menu_container-links">
+                            <Sidebar />
+                        </div>
+                    </div>
                 )}
             </div>
         </div>
--- navbarstyle1/src/containers/login/Login.jsx
+++ navbarstyle1/src/containers/login/Login.jsx
@@ -1,27 +1,100 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './login.css';
 
-const Login = () => {
-    return (
-        <div className="project__login">
-            <form className="project__login-form">
-                <div className="project__login-form_group success">
-                    <label htmlFor="email">Email</label>
-                    <input pattern="[a-zA-Z0-9._+-]+@[a-zA-Z0-9*-]+\.[a-zA-Z]{2,}" type="email" title="must be valid" id="email" required />
-                    <span className="msg">Valid email</span>
+function Login() {
+    const [errorMessages, setErrorMessages] = useState({});
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+    const database = [
+        {
+            username: "username1",
+            password: "password1"
+        },
+        {
+            username: "username2",
+            password: "password2"
+        }
+    ];
+
+    const errors = {
+        uname: "invalid username",
+        pass: "invalid password"
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        var { uname, pass } = document.forms[0];
+
+        const userData = database.find((user) => user.username === uname.value);
+
+        if (userData) {
+            if (userData.password !== pass.value) {
+                setErrorMessages({ name: "pass", message: errors.pass });
+            } else {
+                setIsSubmitted(true);
+            }
+        } else {
+            setErrorMessages({ name: "uname", message: errors.uname });
+        }
+    };
+
+    const renderErrorMessage = (name) =>
+        name === errorMessages.name && (
+            <div className="error">{errorMessages.message}</div>
+        );
+
+    const renderForm = (
+        <div className="form">
+            <form onSubmit={handleSubmit}>
+                <div className="input-container">
+                    <label htmlFor="">Username </label>
+                    <input type="text" name="uname" id="username" required />
+                    {renderErrorMessage("uname")}
                 </div>
-                <div className="project__login-form_group error">
-                    <label htmlFor="password">Password</label>
-                    <input pattern="[a-zA-Z0-9._+-!]{8,}" title="Must be at least 8 characters" type="password" id="password" required />
-                    <span className="project__login-form_group-inputhint">
-                        Must be at least 8 characters
-                    </span>
-                    <span className="msg">Incorrect password</span>
+                <div className="input-container">
+                    <label htmlFor="">Password </label>
+                    <input type="password" name="pass" id="password" required />
+                    {renderErrorMessage("pass")}
                 </div>
-                <button type="submit">Login</button>
+                <div className="button-container">
+                    <input type="submit" value="" />
+                </div>
             </form>
         </div>
-    )
-}
+    );
 
-export default Login
+    return (
+        <div className="login-page">
+            <div className="login-form">
+                <div className="title">Sign In</div>
+                {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
+            </div>
+        </div>
+    );
+
+
+    // const Login = () => {
+    //      return (
+    //          <div className="project__login">
+    //              <form className="project__login-form">
+    //                  <div className="project__login-form_group success">
+    //                      <label htmlFor="email">Email</label>
+    //                      <input pattern="[a-zA-Z0-9._+-]+@[a-zA-Z0-9*-]+\.[a-zA-Z]{2,}" type="email" title="must be valid" id="email" required />
+    //                      <span className="msg">Valid email</span>
+    //                  </div>
+    //                  <div className="project__login-form_group error">
+    //                      <label htmlFor="password">Password</label>
+    //                      <input pattern="[a-zA-Z0-9._+-!]{8,}" title="Must be at least 8 characters" type="password" id="password" required />
+    //                      <span className="project__login-form_group-inputhint">
+    //                          Must be at least 8 characters
+    //                      </span>
+    //                      <span className="msg">Incorrect password</span>
+    //                  </div>
+    //                  <button type="submit">Login</button>
+    //              </form>
+    //          </div>
+    //      )
+     }
+
+    export default Login
